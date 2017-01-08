a2f9081c04fee08b06ee8f297e580dfbe547b11b
Jordi Prats <jprats@systemadmin.es>
2017-01-08 15:37:22 +0100

    directoris

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,13 @@
 class fail2ban(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits fail2ban::params{
+                $manage_package          = true,
+                $package_ensure          = 'installed',
+                $manage_service          = true,
+                $manage_docker_service   = true,
+                $service_ensure          = 'running',
+                $service_enable          = true,
+                $actiond_purge_unmanaged = false,
+                $filterd_purge_unmanaged = false,
+              ) inherits fail2ban::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
